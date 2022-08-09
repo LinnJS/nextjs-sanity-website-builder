4dcdde79437032480b41c1aaaef3bcb0a9a40a43
Justin Linn <justinslinn727@gmail.com>
2022-08-09 00:15:45 -0400

    Add custom action resolver

--- studio/schemas/documents/siteConfig.ts
+++ studio/schemas/documents/siteConfig.ts
@@ -6,8 +6,6 @@ export default {
   name: 'site-config',
   type: 'document',
   title: 'Site configuration',
-  // https://www.sanity.io/docs/experimental/ui-affordances-for-actions
-  __experimental_actions: [/* "create", "delete", */ 'update', 'publish'],
   fieldsets: [{ name: 'footer', title: 'Footer' }],
   fields: [
     {
